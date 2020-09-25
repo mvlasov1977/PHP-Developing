bac72ba217f127e61e8af7d6b5228679b928eced
Mac <mvlasov1977@gmail.com>
2020-09-25 11:12:44 +0300

    Code Sniffer install

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 
-echo "Hello my friend" ;
+require __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается на прямую
+// Он загрузится автоматически благодаря автозагрузке
+use My\Project\Runner;
+
+print_r(Runner\run());
 
